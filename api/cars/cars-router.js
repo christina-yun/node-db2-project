f3a58bdd48dfef9ec948d351d867c068fa4e27d6
Christina Yun <poncheeyun@yahoo.com>
2021-10-05 08:35:48 -0700

    set up cars router skeleton

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,4 +1,26 @@
 const router = require('express').Router();
 const Cars = require('./cars-model');
 
+//[GET] /api/cars
+router.get('/', (req, res, next) => {}) 
+
+//[GET] /api/cars/:id
+router.get('/:id', (req, res, next) => {}) 
+
+//[POST] /api/cars
+router.post('/', (req, res, next) => {})
+
+//**[PUT] /api/cars/:id
+router.put('/:id', (req, res, next) => {})
+
+//**[DELETE] /api/cars/:id
+router.delete('/:id', (req, res, next) => {})
+
+//Error handling
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    });
+});
+
 module.exports = router;
